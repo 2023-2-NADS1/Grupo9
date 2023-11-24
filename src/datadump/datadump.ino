4c5c30e3fc6848f349d3127366097e617f22bffa
Matheus Emiliano De Souza <matheus.souza@meta.com.br>
2023-11-24 14:24:53 -0300

    Inclusao do codigo final

--- src/datadump/datadump.ino
+++ src/datadump/datadump.ino
@@ -1,13 +1,27 @@
+#define BLYNK_TEMPLATE_ID "TMPL2LOnGns6G"
+#define BLYNK_TEMPLATE_NAME "datadump"
+#define BLYNK_AUTH_TOKEN "YiXrm2fV-3t_O35MnPznObj__LBJq6sC"
+
 #include <Ultrasonic.h>
 #include "HX711.h"
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <BlynkSimpleEsp32.h>
 
-const int echoPin = 25; // Pino digital utilizado pelo HC-SR04 Echo (recebe)
+
+#define BLYNK_PRINT Serial
+char auth[] = BLYNK_AUTH_TOKEN;
+
+char ssid[] = "ar";
+char pass[] = "12345678";
+
+const int echoPin = 12; // Pino digital utilizado pelo HC-SR04 Echo (recebe)
 const int trigPin = 14; // Pino digital utilizado pelo HC-SR04 Trig (envia)
 
-const int dtPin = 15;   // Pino digital do ESP32 conectado ao DT do HX711
-const int sckPin = 5;   // Pino digital do ESP32 conectado ao SCK do HX711
+const int dtPin = 18;   // Pino digital do ESP32 conectado ao DT do HX711
+const int sckPin = 4;   // Pino digital do ESP32 conectado ao SCK do HX711
 
-const float profundidade = 18.8;
+const float profundidade = 22;
 const float largura = 11;
 
 Ultrasonic ultrasonic(trigPin, echoPin); // Inicializando os pinos do ESP32 para o sensor ultrassônico
@@ -17,29 +31,23 @@ void setup() {
   Serial.begin(115200); // Inicializa a porta serial
   scale.begin(dtPin, sckPin); // Inicializa o HX711 com os pinos corretos
 
-  // Calibrar a célula de carga
-  Serial.println("Inicie a calibracao. Por favor, coloque a massa conhecida na celula de carga...");
-  delay(2000);
-  scale.set_scale(); // Zera a escala (tara)
-
-  // Coloque a massa conhecida na célula de carga
-  Serial.println("Aguarde 10 segundos...");
-  delay(10000);
-
-  // Obtenha a leitura do HX711 após 10 segundos
-  float peso = scale.get_units(10);
-
-  // Configure a escala do HX711 para gramas
-  scale.set_scale(peso / 1000.0); // Convertendo para gramas
-  Serial.println("Calibracao concluida!");
+  // // Configure a escala do HX711 para gramas
+   scale.set_scale(152.5); 
+    scale.tare();
+    Blynk.begin(auth, ssid, pass);
 }
 
 void loop() {
+  Blynk.run();
   // Mede a distância em centímetros
   float distancia = ultrasonic.read(CM);
 
   // Mede o peso em gramas
   float peso = scale.get_units(10); // 10 leituras para suavizar o resultado
+
+if(peso <= 1) {
+   peso = 0;
+ } 
 
  float volume  = profundidade * largura * distancia;
   Serial.print("Distancia: ");
@@ -50,5 +58,11 @@ void loop() {
   Serial.print(volume/1000);
   Serial.println(" m³");
 
+  Serial.println();
+  Blynk.virtualWrite(V0,peso);
+  Blynk.virtualWrite(V1,distancia);
+  Blynk.virtualWrite(V2,volume/1000);
+ 
   delay(1000); // Aguarda 1 segundo antes da próxima leitura
 }
+
